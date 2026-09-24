99ab38aa44e61189ad47651c632667e0df90d194
Hugo Soriano <873956@unizar.es>
2026-09-24 20:25:35 +0200

    Resultados de Euler-Maruyama

--- src/main.cpp
+++ src/main.cpp
@@ -20,8 +20,8 @@ int main(void) {
  
     parametros.x0 = 1.0;                                            // Posición inicial
     parametros.p0 = 0.0;                                            // Momento inicial
-    parametros.eta = 0.1;                                           // Amortiguamiento: 0.1, 1.0, 10
-    parametros.h = 0.1;                                          // Paso de tiempo: 0.1, 0.001, 0.0001, 0.00001
+    parametros.eta = 10.0;                                           // Amortiguamiento: 0.1, 1.0, 10
+    parametros.h = 0.0001;                                          // Paso de tiempo: 0.1, 0.01, 0.001, 0.0001
     parametros.t_max = 10000.0;                                     // Tiempo máximo de simulación
     parametros.algorithm = Algorithm::EulerMaruyama;                //Alrgoritmo de integración: Euler-Maruyama, Runge-Kutta, Verlet
 
